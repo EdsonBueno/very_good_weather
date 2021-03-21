ae515014680dcc5a4a6c4644a41175a39845a97d
Edson Bueno <ejr.bueno@gmail.com>
2021-03-21 14:49:15 -0300

    create weather components barrel file

---
+++ lib/weather/view/components/components.dart
@@ -0,0 +1,8 @@
+export 'centered_progress_indicator.dart';
+export 'location_text.dart';
+export 'no_location_selected_indicator.dart';
+export 'weather_load_failure_indicator.dart';
+export 'weather_observation_view.dart';
+export 'weather_settings_button.dart';
+export 'weather_status_illustration.dart';
+export 'weather_status_localized_description.dart';
